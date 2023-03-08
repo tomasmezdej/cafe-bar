5576d0c428d544882e44c1b085ea61eaa346bd8f
Tomáš Meždej <tmezdej@gmail.com>
2023-03-08 22:15:42 +0100

    staff and events page sections

--- src/components/contentCards/GalleryCard.jsx
+++ src/components/contentCards/GalleryCard.jsx
@@ -92,7 +92,7 @@ const GalleryCard = () => {
 
     scrollContainer.addEventListener('wheel', (evt) => {
       evt.preventDefault();
-      scrollContainer.scrollLeft += evt.deltaY/50;
+      scrollContainer.scrollLeft += evt.deltaY/20;
     });
   }, [])
 
