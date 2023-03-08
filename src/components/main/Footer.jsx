f2390e5793d0d05c5ca7802be85ca178f2b59edf
Tomáš Meždej <tmezdej@gmail.com>
2023-03-08 11:44:24 +0100

    footer update

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -1,9 +1,152 @@
 import classes from "../../assets/styles/components/footer.module.scss"
+import {
+  BiCopy
+} from "react-icons/bi"
+import {
+  BsFacebook,
+  BsTwitter,
+} from "react-icons/bs"
+import {
+  AiFillInstagram
+} from "react-icons/ai"
+
+
+import { days } from "../../config/openingHours"
 
 const Footer = () => {
+  const address = "Hankovce xxs, 06601 xxxx"
+  const email = "info@maracana.sk"
+  const phone = "1-444-123-4559"
+
+  const handleCopyEmail = () => {
+    navigator.clipboard.writeText(email)
+  }
+
+  const redirect = (dest) => {
+    const link = dest === 'fb'
+      ? "facebooklink"
+      : (dest === "ig")
+        ? "https://www.instagram.com/maracana.hankovce/"
+        : "twitterlink"
+    window.open(link, '_blank')
+  }
+
   return (
-    <div className={classes.footer}>
-      footer
+    <div className={classes.footer_wrap}>
+      <div className={classes.footer}>
+        <div
+          className={classes.footer__opening_hours}
+        >
+          <h2>
+            OPENING HOURS
+          </h2>
+          <div
+            className={classes.footer__opening_hours_wrap}
+          >
+            {
+              days.map(day =>
+                <div
+                  className={classes.footer__opening_hours_day}
+                >
+                  <h4>
+                    { day.day }
+                  </h4>
+                  <h4>
+                    { day.opening }
+                  </h4>
+                </div>
+              )
+            }
+
+          </div>
+        </div>
+        <div
+          className={classes.footer__contact_us}
+        >
+          <div>
+            <h2>
+              Contact us
+            </h2>
+            <div
+              className={classes.footer__contact_us_contact}
+            >
+              <div
+                className={classes.footer__contact_us_contact_element}
+              >
+                <h4>
+                  { address }
+                </h4>
+              </div>
+              <div
+                className={classes.footer__contact_us_contact_element}
+              >
+                <h4>
+                  { email }
+                </h4>
+                <BiCopy
+                  className={classes.footer__contact_us_contact_element_button}
+                  onClick={handleCopyEmail}
+                />
+              </div>
+              <div
+                className={classes.footer__contact_us_contact_element}
+              >
+                <h4>
+                  { phone }
+                </h4>
+              </div>
+            </div>
+          </div>
+          {/* <div>
+            <h2>
+              FOllow us
+            </h2>
+            <div
+              className={classes.footer__contact_us_follow_us}
+            >
+              <BsFacebook
+                className={classes.footer__contact_us_contact_element_button}
+                size="30px"
+                onClick={ () => redirect("fb") }
+              />
+              <AiFillInstagram
+                className={classes.footer__contact_us_contact_element_button}
+                size="30px"
+                onClick={ () => redirect("ig") }
+              />
+              <BsTwitter
+                className={classes.footer__contact_us_contact_element_button}
+                size="30px"
+                onClick={ () => redirect("tw") }
+              />
+            </div>
+          </div> */}
+        </div>
+      </div>
+      <div className={classes.footer_map_wrap}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2616.1328161584674!2d21.947495!3d49.027080899999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473eb3c10ed26783%3A0xa406bc0689142aa9!2sMaracana%20cafe%20%26%20bar%20%26%20ice%20cream!5e0!3m2!1ssk!2suk!4v1678270911787!5m2!1ssk!2suk"
+        className={classes.footer_map}
+        style={{
+          "border":"0",
+          "border-radius": "8px"
+        }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+      </div>
+      {/* <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2616.1328161584674!2d21.947495!3d49.027080899999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473eb3c10ed26783%3A0xa406bc0689142aa9!2sMaracana%20cafe%20%26%20bar%20%26%20ice%20cream!5e0!3m2!1ssk!2suk!4v1678270911787!5m2!1ssk!2suk"
+
+        style={{
+          "border":"0",
+          "border-radius": "8px"
+        }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe> */}
     </div>
   )
 }
---
+++ src/config/openingHours.js
@@ -0,0 +1,30 @@
+export const days = [
+  {
+    day: "Pondelok",
+    opening: "CLOSED"
+  },
+  {
+    day: "Utorok",
+    opening: "16:00 - 22:00"
+  },
+  {
+    day: "Streda",
+    opening: "16:00 - 22:00"
+  },
+  {
+    day: "Štvrtok",
+    opening: "16:00 - 22:00"
+  },
+  {
+    day: "Piatok",
+    opening: "16:00 - 22:00"
+  },
+  {
+    day: "Sobota",
+    opening: "16:00 - 22:00"
+  },
+  {
+    day: "Nedeľa",
+    opening: "16:00 - 22:00"
+  }
+]
